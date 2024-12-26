7ff0af34bd98e4073e17d1aae6d9dc0ba8ba3367
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2024-12-26 22:55:16 +0200

    solve مخلÙe مخلÙe login text problem phase 2

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -47,7 +47,7 @@
                             <div id="username-field" class="field-wrapper input">
                                 <label for="phone">رقم التلفون</label>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
-                                <input id="phone" name="phone" type="text" class="form-control" placeholder="e.g 01234567890">
+                                <input id="phone" name="phone" type="text" class="form-control" placeholder="ادحل رقم الهاتف">
                             </div>
 
                             <div id="password-field" class="field-wrapper input mb-2">
