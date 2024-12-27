5c3783909ad0f1f932d0148cf3b74b7d5763c676
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2024-12-27 13:55:05 +0200

    update factory seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,19 +31,19 @@ class DatabaseSeeder extends Seeder
             'phone' => '01270989676',
             'password' => bcrypt('2510885891')
         ]);
-        Rout::factory(5)->create();
-        User::factory()->create([
-            'name' => 'User Example',
-            'phone' => '01234567890',
-            'id_number' => '30302022402727',
-            'password' => bcrypt('12345678'),
-            'active' => true,
-            'rout_id' => Rout::inRandomOrder()->first()->id,
-        ]);
-        Gov::factory(20)->create();
-        City::factory(80)->create();
-        User::factory(20)->create();
-        Product::factory(10)->create();
-        Order::factory(1000)->create();
+//        Rout::factory(5)->create();
+//        User::factory()->create([
+//            'name' => 'User Example',
+//            'phone' => '01234567890',
+//            'id_number' => '30302022402727',
+//            'password' => bcrypt('12345678'),
+//            'active' => true,
+//            'rout_id' => Rout::inRandomOrder()->first()->id,
+//        ]);
+//        Gov::factory(20)->create();
+//        City::factory(80)->create();
+//        User::factory(20)->create();
+//        Product::factory(10)->create();
+//        Order::factory(1000)->create();
     }
 }
